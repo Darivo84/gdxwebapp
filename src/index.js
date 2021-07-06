f9b5addbf35dc96b326d888c106404ef16c7bb5d
Dayne <dayne.voller@outlook.com>
2021-07-06 12:41:01 +0200

    indexjs update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/gdx">
+    <Router basename="/gdxwebapp">
       <App />
     </Router>
   </React.StrictMode>,
