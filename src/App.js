4dbad545d6e568d3c546cd1eb31dd3d362f0511a
Dayne <dayne.voller@outlook.com>
2021-07-07 10:32:10 +0200

    more link updates

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route path="/gdxwebapp/welcome" component={Welcome} />
+      <Route path="/welcome" component={Welcome} />
     </Switch>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/gdxwebapp">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
