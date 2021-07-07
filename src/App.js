5a22ac27b593aa5c3ff834dd32fdc65d79990a77
Dayne <dayne.voller@outlook.com>
2021-07-07 16:07:39 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Page Components
 import Home from './pages/Home';
@@ -7,12 +7,12 @@ import Welcome from './pages/Welcome';
 
 function App() {
   return (
-    <HashRouter basename="/">
+    <Router basename="/">
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path={process.env.PUBLIC_URL + '/welcome'} component={Welcome} />
+        <Route path="/welcome" component={Welcome} />
       </Switch>
-    </HashRouter>
+    </Router>
   );
 }
 
