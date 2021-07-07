1f0e9c19be72b6160e7637edc2a351950b728ad6
Dayne <dayne.voller@outlook.com>
2021-07-07 16:23:23 +0200

    HashRouter added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Page Components
 import Home from './pages/Home';
