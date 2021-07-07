1ba19ed09f16ae0f14f1469916cd9f5108bd8c67
Dayne <dayne.voller@outlook.com>
2021-07-07 10:51:28 +0200

    Router changes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/gdxwebapp">
+    <Router basename="/gdxwebapp" history={history}>
       <App />
     </Router>
   </React.StrictMode>,
